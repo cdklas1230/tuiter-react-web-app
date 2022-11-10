fa723c5a51ac8ba6a4ac744b07757f2af20c21ad
liuxinyao <liu.xinyao@northeastern.edu>
2022-11-10 00:17:12 -0800

    a8 REST sp22

--- src/tuiter/tuits/index.js
+++ src/tuiter/tuits/index.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, {useEffect} from "react";
 import TuitItem from "./tuit-item.js";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {findTuitsThunk}
+    from "../../services/tuits-thunks";
 
 const TuitsList = () => {
-    const tuitsArray = useSelector(state => state.tuits)
+    const {tuits, loading} = useSelector(state => state.tuitsData)
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(findTuitsThunk())
+    }, [dispatch])
+
     return(
         <ul className="list-group">
             {
-                tuitsArray.map(tuits =>
+                loading &&
+                <li className="list-group-item">
+                    Loading...
+                </li>
+            }
+            {
+                tuits.map(tuits =>
                     <TuitItem
                         key={tuits._id} post={tuits}/> )
             }
--- src/tuiter/tuits/tuit-item.js
+++ src/tuiter/tuits/tuit-item.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {useDispatch} from "react-redux";
-import {deleteTuit} from "./tuits-reducer";
+import {deleteTuitThunk, updateTuitThunk} from "../../services/tuits-thunks";
 
 const TuitItem = (
     {
@@ -12,6 +12,8 @@ const TuitItem = (
             "replies": 123,
             "retuits": 432,
             "likes": 2345,
+            "disliked": true,
+            "dislikes": 0,
             "handle": "spacex",
             "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars"
         }
@@ -19,7 +21,7 @@ const TuitItem = (
 ) => {
     const dispatch = useDispatch();
     const deleteTuitHandler = (id) => {
-        dispatch(deleteTuit(id));
+        dispatch(deleteTuitThunk(id));
     }
     return (
         <li className="list-group-item">
@@ -42,25 +44,56 @@ const TuitItem = (
                         </div>
                         <div className="tweet-title ms-2">{post.tuit}</div>
                     </div>
-                    <div className="row mt-2">
-                        <div className="col-3">
+                    <div className="row mt-2 ms-1">
+                        <div className="col-2">
                             <i className="bi bi-chat"></i> &nbsp; {post.replies}
                         </div>
-                        <div className="col-3">
+                        <div className="col-2">
                             <i className="bi bi-repeat"></i> &nbsp; {post.retuits}
                         </div>
                         {
                             post.liked ? (
-                                <div className="col-3">
-                                    <i className="bi bi-heart-fill" style={{color: "red"}}></i> &nbsp; {post.likes}
+                                <div className="col-2">
+                                    <i onClick={() => dispatch(updateTuitThunk({
+                                        ...post,
+                                        liked: false,
+                                        likes: post.likes - 1
+                                    }))} className="bi bi-heart-fill" style={{color: "red"}}></i>
+                                    &nbsp; {post.likes}
                                 </div>
                             ) : (
-                                <div className="col-3">
-                                    <i className="bi bi-heart"></i> &nbsp; {post.likes}
+                                <div className="col-2">
+                                    <i onClick={() => dispatch(updateTuitThunk({
+                                        ...post,
+                                        liked: true,
+                                        likes: post.likes + 1
+                                    }))} className="bi bi-heart"></i>
+                                    &nbsp; {post.likes}
                                 </div>
                             )
                         }
-                        <div className="col-3">
+                        {
+                            post.disliked ? (
+                                <div className="col-2">
+                                    <i onClick={() => dispatch(updateTuitThunk({
+                                        ...post,
+                                        disliked: false,
+                                        dislikes: post.dislikes - 1
+                                    }))} className="bi bi-hand-thumbs-down-fill" style={{color: "black"}}></i>
+                                    &nbsp; {post.dislikes}
+                                </div>
+                            ) : (
+                                <div className="col-2">
+                                    <i onClick={() => dispatch(updateTuitThunk({
+                                        ...post,
+                                        disliked: true,
+                                        dislikes: post.dislikes + 1
+                                    }))} className="bi bi-hand-thumbs-down"></i>
+                                    &nbsp; {post.dislikes}
+                                </div>
+                            )
+                        }
+                        <div className="col-2">
                             <i className="bi bi-upload"></i>
                         </div>
                     </div>
